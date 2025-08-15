9fa35336cd4f1e6d6144655a602db8feb14a0d76
Ekaterina Vazhdaeva <vazhdaichik95@yandex.ru>
2025-08-15 16:18:46 +0300

    Работа выполнена

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sp6-2_smart-table_starter
+https://github.com/Vazhdaichik95/smart-table.git
--- src/components/filtering.js
+++ src/components/filtering.js
@@ -2,13 +2,35 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 
+const compare = createComparison(defaultRules);
+
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+
+    Object.keys(indexes)
+          .forEach((elementName) => {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName])
+                         .map(name => {
+                            const opt = document.createElement('option');
+                            opt.value=name;
+                            opt.textContent=name;
+                            return opt;
+                         })
+            )
+          });
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
 
+        if (action && action.name === 'clear') {
+            const input = action.parentElement.querySelector('input');
+            input.value = '';
+            state[action.dataset.field] = '';
+        }
+
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+
+        return data.filter(row => compare(row, state));
     }
 }
