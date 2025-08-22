9a692c92473a5128eb35e7ee089c92ca7af4aa2b
Ekaterina Vazhdaeva <vazhdaichik95@yandex.ru>
2025-08-22 23:22:46 +0300

    Шаг 4 выполнен

--- src/main.js
+++ src/main.js
@@ -54,6 +54,8 @@ async function render(action) {
     result = applyPagination(result, state, action);
     */
 
+    query = applySearching(query, state, action);
+
     query = applyFiltering(query, state, action);
 
     query = applyPagination(query, state, action);
